0d737f82e4853508db54342bae6d84065215d9a4
AnushaV1 <anush_chander@yahoo.com>
2020-10-23 11:46:16 -0700

    backend pointed to heroku

--- src/JoblyApi.js
+++ src/JoblyApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001";
-
+const BASE_URL = process.env.REACT_APP_BACKEND_URL ||  'http://anusha-jobly.herokuapp.com';
+//"http://localhost:3001";
 class JoblyApi {
 
       static async request(endpoint, paramsOrData = {}, verb = "get") {
